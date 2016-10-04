f7a136bf33349399a1dbe333a33a69f230920a06
Sabbir Ahmed <sabbiirr@gmail.com>
2016-10-04 23:13:55 +0600

    basics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
 import fs from 'fs-extra';
 import os from 'os';
 import path from 'path';
+import nginmix from 'commander';
+
+//Listen
+
+nginmix.on('--help', () => {
+  console.log(
+    `
+    nginmix verison ${nginmix.version()}
+    `
+  );
+});
+
+// Version number
+nginmix
+  .version('0.0.1');
+
+// Usage
+nginmix
+  .usage('-n [domain name] -p [port to pass] -h [host]');
+
+// Options
+nginmix
+  .option('-n, --name', 'Specify a domain name / Server Name')
+  .option('-p, --port', 'Port number where your app is running')
+  .option('-h, --host', 'Host where your sever will listening (Default 80)')
+  .option('-e, --enabled', "List enabled sites")
+  .option('-a, --available', "List available sites")
+  .option('-v, --version', "nginmix version");
+
+nginmix
+  .parse(process.argv);
+
+
 
